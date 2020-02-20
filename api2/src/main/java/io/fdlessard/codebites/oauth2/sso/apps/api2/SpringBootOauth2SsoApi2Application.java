63ac3a4aee97f4774147839faa9803da6d85ad7b
Francois-David Lessard <francois-david.lessard@nbc.ca>
2020-02-20 11:12:29 -0500

    Cleanup

--- api2/src/main/java/io/fdlessard/codebites/oauth2/sso/apps/api2/SpringBootOauth2SsoApi2Application.java
+++ api2/src/main/java/io/fdlessard/codebites/oauth2/sso/apps/api2/SpringBootOauth2SsoApi2Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootOauth2SsoApiApplication {
+public class SpringBootOauth2SsoApi2Application {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBootOauth2SsoApi2Application.class, args);
